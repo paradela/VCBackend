25e7cde5c6729ac264efb9d3bc2d57da95c781f5
Ricardo Paradela <rem.paradela@gmail.com>
2015-04-21 01:15:46 +0100

    Dont know why cardmanager was out...

---
+++ VCBackend/Business Rules/VCards/TSMFakeProxy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VCBackend.Models;
+using VCBackend.Repositories;
+
+namespace VCBackend.Business_Rules.VCards
+{
+    public class TSMFakeProxy
+    {
+        public static int ERROR = 0;
+        private static readonly byte[] emptyCard = new byte[]
+        {
+            0x60, 0x02, 0x01, 0xE4, 0x08, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        };
+
+        private VCardManager CardManager;
+        private UnitOfWork UnitOfWork;
+
+        public TSMFakeProxy()
+        {
+            UnitOfWork = new UnitOfWork();
+            CardManager = new VCardManager(UnitOfWork);
+        }
+
+        private byte[] INT2LE(int data)
+        {
+            byte[] b = new byte[4];
+            b[0] = (byte)data;
+            b[1] = (byte)(((uint)data >> 8) & 0xFF);
+            b[2] = (byte)(((uint)data >> 16) & 0xFF);
+            b[3] = (byte)(((uint)data >> 24) & 0xFF);
+            return b;
+        }
+
+        public int InstallCard(int AccountId)
+        {
+            VCard card = CardManager.CreateCard(AccountId, emptyCard);
+            if (card != null)
+                return card.Id;
+            else return ERROR;
+        }
+
+        public bool InitCard(int CardId)
+        {
+            if (CardId != 0)
+                return CardManager.InitCard(CardId, INT2LE(CardId));
+            else return false;
+        }
+    }
+}
---
+++ VCBackend/Business Rules/VCards/VCardManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VCBackend.Repositories;
+using VCBackend.Models;
+using VCBackend.Business_Rules.Accounts;
+using VCBackend.Utility.Time;
+
+namespace VCBackend.Business_Rules.VCards
+{
+    public class VCardManager : Manager
+    {
+        public VCardManager(UnitOfWork UnitOfWork)
+            : base(UnitOfWork)
+        {
+        }
+
+        //Create Card
+        public VCard CreateCard(int AccountId, byte[] EmptyCard)
+        {
+            AccountManager manager = new AccountManager(UnitOfWork);
+            VCard card = new VCard(EmptyCard);
+
+            UnitOfWork.VCardRepository.Add(card);
+            if (!manager.SetCardToAccount(AccountId, card))
+            {
+                UnitOfWork.VCardRepository.Delete(card);
+                return null;
+            }
+            UnitOfWork.Save();
+            return card;
+        }
+        
+        //Init Card (Add Serial number)
+        public bool InitCard(int CardId, byte[] SerialNumber)
+        {
+            VCard card = UnitOfWork.VCardRepository.GetByID(CardId);
+            if (card != null || SerialNumber.Length == 4)
+            {
+                if (card.Write(6, SerialNumber, (uint)SerialNumber.Length))
+                {
+                    UnitOfWork.VCardRepository.Update(card);
+                    UnitOfWork.Save();
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+        //Get Card
+        public VCard GetCardById(int CardId)
+        {
+            VCard card = UnitOfWork.VCardRepository.GetByID(CardId);
+            if (card == null)
+            {
+                throw new VCardException("Card ID unknown.");
+            }
+            return card;
+        }
+
+        //Delete Card
+        public void DeleteCardById(int CardId)
+        {
+            VCard card = UnitOfWork.VCardRepository.GetByID(CardId);
+            if (card == null)
+            {
+                throw new VCardException("Card ID unknown.");
+            }
+            UnitOfWork.VCardRepository.Delete(card);
+            UnitOfWork.Save();
+        }
+
+        public VCardToken CreateTokenFromCard(int CardId, int AccountId)
+        {
+            VCard card = UnitOfWork.VCardRepository.GetByID(CardId);
+            Account account = UnitOfWork.AccountRepository.GetByID(AccountId);
+            if (card == null) throw new VCardException("Card ID unknown.");
+            if (account == null) throw new VCardException("Account ID unknown");
+            byte[] cardData = new byte[64];
+            byte[] cardHeader = card.Read(0, 10); //Read the card header till the S/N.
+            System.Array.Copy(cardHeader, cardData, 10);
+            VCardToken token = new VCardToken(cardData);
+            token.AccountId = card.AccountId;
+            token.Validity = Time.ValidityInHours(24);
+            account.VCardToken.Add(token);
+            UnitOfWork.VCardTokenRepository.Add(token);
+            UnitOfWork.AccountRepository.Update(account);
+            UnitOfWork.Save();
+            return token;
+        }
+    }
+}
