835b19da69ef0b72e9c7403102eea0df2f403b5f
Ricardo Paradela <rem.paradela@gmail.com>
2015-05-29 19:18:49 +0100

    Added tkmsg load command to load cards and tokens!

--- VCBackend/ExternalServices/Ticketing/Card4BTicketingKernelProxy.cs
+++ VCBackend/ExternalServices/Ticketing/Card4BTicketingKernelProxy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace VCBackend.ExternalServices.Ticketing
 {
@@ -17,34 +18,55 @@ namespace VCBackend.ExternalServices.Ticketing
             Client = new HttpClient(); 
         }
 
-        public void LoadProduct(String ProdId, VCard Card)
+        public bool LoadCard(VCard Card, uint Ammount, uint LoadId)
         {
-            LoadProductEx(ProdId, Card: Card);
+            if (Ammount >= 500)
+            {
+                string tkmsg = 
+                    "<tkmsg><load>" +
+                    "<product card_value=\"2065208\"><attribs>" +
+                    "<attrib type=\"stored_value\">" + Ammount + "</attrib>" +
+                    "<attrib type=\"sale_device\">42</attrib>" +
+                    "<attrib type=\"sale_date\">" + DateTime.Today.ToString("yyyy-MM-dd") + "</attrib>" +
+                    "<attrib type=\"sale_number_daily\">" + LoadId + "</attrib>" +
+                    "</attribs></product></load></tkmsg>";
+                return LoadProductEx(tkmsg, Card: Card).Result;
+            }
+            else return false;
         }
 
-        public void LoadProduct(String ProdId, VCardToken Token)
+        public bool LoadToken(VCardToken Token, uint LoadId)
         {
-            LoadProductEx(ProdId, Token: Token);
+            string tkmsg =
+                "<tkmsg><load>" +
+                "<product card_value=\"2065209\"><attribs>" +
+                "<attrib type=\"stored_value\">100</attrib>" +
+                "<attrib type=\"sale_device\">42</attrib>" +
+                "<attrib type=\"sale_date\">" + DateTime.Today.ToString("yyyy-MM-dd") + "</attrib>" +
+                "<attrib type=\"sale_number_daily\">" + LoadId + "</attrib>" +
+                "<attrib type=\"date_initial\">" + DateTime.Today.ToString("yyyy-MM-dd HH:mm") + "</attrib>" +
+                "</attribs></product></load></tkmsg>";
+            return LoadProductEx(tkmsg, Token: Token).Result;
         }
 
-        private async void LoadProductEx(String ProdId, VCard Card = null, VCardToken Token = null)
+        private async Task<bool> LoadProductEx(String TKMsg, VCard Card = null, VCardToken Token = null)
         {
             //Call WS to get a server to load the card
             var callTask = Client.GetAsync(ServerUri);
 
             var card = new JObject();
 
-            if (Card == null)
+            if (Card != null)
             {
                 card["type"] = "CTS512B";
-                card["data"] = "carddadatasdasda";//Card.Data;
+                card["data"] = Card.Data;//Card.Data;
             }
             else if (Token != null)
             {
                 card["type"] = "CTS512B";
                 card["data"] = Token.Data;
             }
-            else return;
+            else return false;
 
             await callTask; //wait for call to be finnished
 
@@ -53,11 +75,12 @@ namespace VCBackend.ExternalServices.Ticketing
             string tkUri = jsonRsp["URL"].ToString();
 
             var jsonReq = new JObject();
-            jsonReq["tkmsg"] = "<tkmsg><product>" + ProdId + "</product></tkmsg>";
+            jsonReq["tkmsg"] = TKMsg;
             jsonReq["card"] = card;
             var response = Client.PostAsJsonAsync(tkUri, jsonReq).Result;
             strRsp = response.Content.ReadAsStringAsync().Result;
             jsonRsp = JObject.Parse(strRsp);
+            return true;
         }
     }
 }
